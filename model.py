1e68d72e656f098ae913f87f90a3235cc122544f
Hardianto T.S <111510893+hardiantots@users.noreply.github.com>
2023-06-07 10:58:53 +0800

    Update model.py

--- model.py
+++ model.py
@@ -17,12 +17,13 @@ from torchvision.models import ResNet18_Weights
 
 @st.cache_resource
 def load_model(model_path):
-    model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+    device = 'cuda' if torch.cuda_isavailable() else 'cpu'
+    model = models.resnet18(weights=ResNet18_Weights.DEFAULT, map_location=device)
     # Set the output according to the number of existing class
     num_features = model.fc.in_features
     model.fc = nn.Linear(num_features, 4)
     
-    model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(torch.load(model_path), map_location=device)
     model.eval()
 
     return model
